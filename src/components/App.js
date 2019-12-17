f0db203ce97b2944a725fd93afbb13b6064b1b05
Dj <ritchey0713@gmail.com>
2019-12-17 12:58:03 -0500

    change to class

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,26 @@
 import React from 'react';
 import SearchBar from './SearchBar';
+import youtube, {baseParams} from "../APIs/youtube"
 
 
-function App() {
-  return (
-    <div className="App ui container">
-      video finder
-      <SearchBar />
-    </div>
-  );
+class App extends React.Component {
+  onSearchSubmit = (searchTerm) => {
+    youtube.get('/search', {
+      params: {
+        ...baseParams,
+        q: searchTerm
+      }
+    })
+  }
+
+  render(){
+    return (
+      <div className="App ui container">
+        video finder
+        <SearchBar onSearchSubmit={this.onSearchSubmit}/>
+      </div>
+    );
+  }
 }
 
 export default App;
