93b76675ac18eea8e25c40e26f402d23a2aa47e8
Dj <ritchey0713@gmail.com>
2019-12-17 12:11:55 -0500

    add classes

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import SearchBar from './SearchBar';
+
+
+function App() {
+  return (
+    <div className="App ui container">
+      video finder
+      <SearchBar />
+    </div>
+  );
+}
+
+export default App;
