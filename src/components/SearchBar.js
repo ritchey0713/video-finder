6b61183bb1ee97eef194cd3a4f37dedb7794d72e
Dj <ritchey0713@gmail.com>
2019-12-17 12:11:48 -0500

    build search bar

---
+++ src/components/SearchBar.js
@@ -0,0 +1,35 @@
+import React from "react";
+
+class SearchBar extends React.Component{
+  
+  state = {
+    searchTerm: ""
+  }
+
+  onSearchChange = (e) => {
+    this.setState({
+      searchTerm: e.target.value
+    })
+  }
+
+  onFormSubmit = (e) => {
+    e.preventDefault()
+    console.log("submit")
+    // TODO: callback from parent 
+  }
+
+  render(){
+    return(
+      <div className="search-bar ui segment">
+        <form className="ui form" onSubmit={this.onFormSubmit}>
+          <div className="field">
+            <label>Video Search</label>
+            <input type="text" value={this.state.searchTerm} onChange={this.onSearchChange}/>
+          </div>
+        </form>
+      </div>
+    )
+  }
+}
+
+export default SearchBar
