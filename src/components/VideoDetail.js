e41376f904f00033095095837aaed423965315fd
Dj <ritchey0713@gmail.com>
2019-12-18 13:30:47 -0500

    fix bug

--- src/components/VideoDetail.js
+++ src/components/VideoDetail.js
@@ -1,14 +1,19 @@
 import React from "react"
+import parse from "html-react-parser"
 
 const VideoDetail = (props) => {
   if(!props.video){
     return <div>Loading...</div>
   } else {
+    const title = parse(props.video.snippet.title)
     return (
       <div>
+        <div className="ui embed">
+          <iframe src={`https://www.youtube.com/embed/${props.video.id.videoId}`} title={props.video.snippet.title}/>
+        </div>
         <div className=" ui segment">
           <h4 className="ui header">
-            {props.video.snippet.title}
+            {title}
           </h4>
           <p>
             {props.video.snippet.description}
