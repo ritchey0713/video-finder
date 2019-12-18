90897644f25cc33a668fd37930c79ee4172c26fa
Dj <ritchey0713@gmail.com>
2019-12-18 12:44:05 -0500

    add state

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,32 @@
 import React from 'react';
 import SearchBar from './SearchBar';
 import youtube, {baseParams} from "../APIs/youtube"
+import VideoList from './VideoList';
+import VideoDetail from "./VideoDetail"
 
 
 class App extends React.Component {
-  onSearchSubmit = (searchTerm) => {
-    youtube.get('/search', {
+  state = {
+    videos: [],
+    selectedVideo: null
+  }
+
+
+  onSearchSubmit = async (searchTerm) => {
+    const resp = await youtube.get('/search', {
       params: {
         ...baseParams,
         q: searchTerm
       }
+    })
+    this.setState({
+      videos: resp.data.items
+    })
+  }
+
+  onVideoSelect = (video) => {
+    this.setState({
+      selectedVideo: video
     })
   }
 
@@ -17,7 +34,9 @@ class App extends React.Component {
     return (
       <div className="App ui container">
         video finder
-        <SearchBar onSearchSubmit={this.onSearchSubmit}/>
+        <SearchBar onSearchSubmit={this.onSearchSubmit} />
+        <VideoDetail video={this.state.selectedVideo} />
+        <VideoList videos={this.state.videos} onVideoSelect={this.onVideoSelect} />
       </div>
     );
   }
