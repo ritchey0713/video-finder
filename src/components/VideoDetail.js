7180e74e5e5ce307cc41374699a66684b3fcd860
Dj <ritchey0713@gmail.com>
2019-12-18 12:43:59 -0500

    add comp

---
+++ src/components/VideoDetail.js
@@ -0,0 +1,23 @@
+import React from "react"
+
+const VideoDetail = (props) => {
+  if(!props.video){
+    return <div>Loading...</div>
+  } else {
+    return (
+      <div>
+        <div className=" ui segment">
+          <h4 className="ui header">
+            {props.video.snippet.title}
+          </h4>
+          <p>
+            {props.video.snippet.description}
+          </p>
+        </div>
+      </div>
+    )
+
+  }
+}
+
+export default VideoDetail
